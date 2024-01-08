bf7904d87e8b7464571ee30ef2cca9edef8fca65
ObvMamut <130008535+ObvMamut@users.noreply.github.com>
2024-01-08 15:53:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def login():
     name=request.form['username']
     pwd=request.form['password']
 
-    client = pronotepy.Client('https://e048000a.index-education.net/pronote/eleve.html',
+    client = pronotepy.Client('#YOUR SCHOOLS PRONOTE ADRESS',
                                   username=name,
                                   password=pwd)
 
